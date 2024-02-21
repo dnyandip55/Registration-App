40445b041255212d4db5830eebc5088beb8fb0cb
Dnyandip Jadhav <jadhavvg77@gmail.com>
2024-02-21 22:12:04 +0530

    Splash Screen Update

--- app/src/main/java/com/example/registrationapp/MainActivity.kt
+++ app/src/main/java/com/example/registrationapp/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.registrationapp
 
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.os.Handler
+
 
 class MainActivity : AppCompatActivity() {
+    private val SPLASH_DURATION: Long = 3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btn=findViewById<Button>(R.id.button)
 
-        btn.setOnClickListener(){
-            val i=Intent(this,SignUp::class.java)
-            startActivity(i)
-        }
+        Handler().postDelayed({
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_DURATION)
 
     }
 }
